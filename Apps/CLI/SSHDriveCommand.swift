3665801dd16cc865b8c387ac370fb32c9f099d4e
alecdwm <2741569+alecdwm@users.noreply.github.com>
2026-09-09 22:22:44 +1000

    Bump to 0.1.4

--- Apps/CLI/SSHDriveCommand.swift
+++ Apps/CLI/SSHDriveCommand.swift
@@ -19,7 +19,7 @@ struct SSHDrive: ParsableCommand {
             Docs: https://github.com/alecdwm/sshdrive
             Run `sshdrive doctor` if a location does not appear in Finder.
             """,
-        version: "0.1.0 (milestone 10)",
+        version: "0.1.4",
         subcommands: [
             Add.self, ListCommand.self, Show.self, Status.self, SetCommand.self,
             Mount.self, Unmount.self, Remove.self, AcceptDeletions.self,
